24d279e9ab88461ba36a3673075bfc072f6b5da9
Adil Benseddik <adil.benseddik@gmail.com>
2018-04-11 23:28:32 +0200

    Moving from 5.1 to 5.2 => ALL GREEN :^{P

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180401124056) do
+ActiveRecord::Schema.define(version: 2018_04_01_124056) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
   enable_extension "pgcrypto"
+  enable_extension "plpgsql"
 
   create_table "event_store_events", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "event_type", null: false
